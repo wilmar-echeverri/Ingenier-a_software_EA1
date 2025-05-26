4069246394f6e4c23c9319b893f6eb29f92088e0
wilmar-echeverri <wilmar.echeverri@est.iudigital.edu.co>
2025-05-25 21:59:25 -0500

    Add files via upload

---
+++ Database/Models/Usuario.py
@@ -0,0 +1,24 @@
+from ..Connection import get_connection
+
+class Usuario:
+    def __init__(self, nombre, telefono, tipo_suscripcion):
+        self.nombre = nombre
+        self.telefono = telefono
+        self.tipo_suscripcion = tipo_suscripcion
+
+    def registrar_usuario(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO Usuario (Nombre, Telefono, Tipo_Suscripcion) VALUES (?, ?, ?)",
+                       (self.nombre, self.telefono, self.tipo_suscripcion))
+        conn.commit()
+        conn.close()
+
+    @staticmethod
+    def obtener_todos():
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT Nombre FROM Usuario")
+        usuarios = [row[0] for row in cursor.fetchall()]
+        conn.close()
+        return usuarios
